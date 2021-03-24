57404899dc574962c40a622545b2b92a15785791
Marius <marius.andre.aardal@gmail.com>
2021-03-24 16:42:18 +0000

    pmull32arch alg

--- main.c
+++ main.c
@@ -17,10 +17,10 @@ void print_poly(poly64x2_t p) {
 				}
 				wasFirst = 0;
 				
-				if (i == 1 && j == 0) {
+				if (i == 0 && j == 0) {
 					printf("1");
 				} else {
-					printf("z^%d", (1-i)*64 +j);
+					printf("z^%d", (i)*64 +j);
 				}
 			}
 			c /= 2;
@@ -30,6 +30,7 @@ void print_poly(poly64x2_t p) {
 	if (wasFirst) {
 		printf("0");
 	}
+	printf("\n");
 }
 
 uint64_t rand_uint64() {
@@ -49,25 +50,55 @@ poly64x2_t rand_poly() {
 	// 2^63-1 = 01111111...
 	long c = 9223372036854775807;
 	
-	poly64_t p1 = rand_uint64() & c;
-	poly64_t p2 = rand_uint64();
+	poly64_t p1 = rand_uint64();
+	poly64_t p2 = rand_uint64() & c;
 	
 	poly64x2_t p = {p1, p2};
 	
 	return p;
 }
 
-//poly128_t pmullTheirs64(poly128_t a, poly128_t b) {
-//	poly128_t z = 0;
-//	vmull_p64
-//}
+typedef struct {
+	poly64x2_t p0, p1;
+} pmullres;
 
+pmullres pmull32(poly64x2_t a, poly64x2_t b) {
+	poly64x2_t t = {0,0};
+	pmullres r;
+	r.p0 = (poly64x2_t) vreinterpretq_u64_p128(vmull_p64(a[0], b[0]));
+	r.p1 = (poly64x2_t) vreinterpretq_u64_p128(vmull_p64(a[1], b[1]));
+	t[1] = (poly64_t) veor_u64((uint64x1_t) b[0], (uint64x1_t) b[1]);
+	t[0] = (poly64_t) veor_u64((uint64x1_t) a[0], (uint64x1_t) a[1]);
+	t = (poly64x2_t) vreinterpretq_u64_p128(vmull_p64(t[1], t[0]));
+	t = (poly64x2_t) veorq_u64((uint64x2_t) t, (uint64x2_t) r.p0);
+	t = (poly64x2_t) veorq_u64((uint64x2_t) t, (uint64x2_t) r.p1);
+	r.p0[1] = (poly64_t) veor_u64((uint64x1_t) r.p0[1], (uint64x1_t) t[0]);
+	r.p1[0] = (poly64_t) veor_u64((uint64x1_t) r.p1[0], (uint64x1_t) t[1]);
+	
+	return r;
+}
 
+static inline uint64_t read_pmccntr() {
+		uint64_t val;
+		asm volatile("mrs %0, pmccntr_el0" : "=r"(val));
+		return val;
+}
 
 int main() {
-	//print_poly(rand_poly());
-	poly128_t r = 5;
-	//poly64x2_t castedR = (poly64x2_t) vreinterpretq_u64_p128(r);
+	//printf("Cycle counter: %u ", read_pmccntr());
+	poly64x2_t a = {11, 0};
+	poly64x2_t b = {6, 0};
+	pmullres res = pmull32(a, b);
+	print_poly(a);
+	print_poly(b);
+	print_poly(res.p0);
+	/*
+	print_poly(rand_poly());
+	poly64x2_t r = {5, 0};
+	print_poly(r);
+	printf("\n");
+	poly64x2_t castedR = (poly64x2_t) vreinterpretq_u64_p128(5);
+	print_poly(castedR);*/
 	//poly128_t res = vmull_p64(castedR, castedR);
 	return 0;
 }
